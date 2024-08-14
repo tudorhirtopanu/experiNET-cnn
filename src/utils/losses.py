d07c6e56076c22a857358504e019ce1ccab99e99
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-08-14 14:51:12 +0300

    add mean squared error and its prime

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -27,3 +27,34 @@ def binary_cross_entropy_prime(y_true, y_pred):
         The gradient of the loss with respect to y_pred. This is used for backpropagation during training.
     """
     return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
+
+
+def mse(y_true, y_pred):
+    """
+    Calculate the Mean Squared Error loss between true values and predicted values.
+
+    :param y_true: numpy array
+        The true values. These are the actual target values.
+    :param y_pred: numpy array
+        The predicted values from the model.
+    :return: float
+        The mean squared error loss calculated over all samples. This value represents
+        the average of the squared differences between the true and predicted values.
+    """
+    return np.mean(np.power(y_true - y_pred, 2))
+
+
+def mse_prime(y_true, y_pred):
+    """
+    Calculate the derivative of the Mean Squared Error loss with respect to the predicted values.
+
+    :param y_true: numpy array
+        The true values. These are the actual target values.
+    :param y_pred: numpy array
+        The predicted values from the model.
+    :return: numpy array
+        The gradient of the loss with respect to y_pred. This is used for backpropagation
+        during training to update the weights. The gradient indicates how the loss
+        would change with a small change in the predicted values.
+    """
+    return 2 * (y_pred - y_true) / np.size(y_true)
