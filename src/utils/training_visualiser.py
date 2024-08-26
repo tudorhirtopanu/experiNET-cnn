a3d076aaf9f90523bfe79f6bfd6db21f7e0ab219
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-08-26 23:09:36 +0100

    plot validation error if provided

--- src/utils/training_visualiser.py
+++ src/utils/training_visualiser.py
@@ -4,26 +4,22 @@ import matplotlib.pyplot as plt
 class TrainingVisualiser:
     def __init__(self):
         """
-        Initialize the visualizer with empty lists for batch and epoch errors.
+        Initialize the visualizer with empty lists for batch errors, training epoch errors,
+        and validation epoch errors.
         """
-        self.batch_errors = []
-        self.epoch_errors = []
+        self.train_epoch_errors = []
+        self.val_epoch_errors = []
 
-    def record_batch_error(self, error):
+    def record_epoch_error(self, train_error, val_error=None):
         """
-        Record the batch error.
+        Record the epoch error for both training and validation.
 
-        :param error: The error to record.
+        :param train_error: The training error to record.
+        :param val_error: The validation error to record (optional).
         """
-        self.batch_errors.append(error)
-
-    def record_epoch_error(self, error):
-        """
-        Record the epoch error.
-
-        :param error: The error to record.
-        """
-        self.epoch_errors.append(error)
+        self.train_epoch_errors.append(train_error)
+        if val_error is not None:
+            self.val_epoch_errors.append(val_error)
 
     def plot_errors(self):
         """
@@ -32,8 +28,12 @@ class TrainingVisualiser:
         plt.figure(figsize=(12, 6))
 
         # Plot epoch errors
-        epochs = range(1, len(self.epoch_errors) + 1)
-        plt.plot(epochs, self.epoch_errors, label='Epoch Error', linewidth=2, color='orange')
+        epochs = range(1, len(self.train_epoch_errors) + 1)
+        plt.plot(epochs, self.train_epoch_errors, label='Epoch Error', linewidth=2, color='orange')
+
+        # Plot validation epoch errors if available
+        if len(self.val_epoch_errors)>0:
+            plt.plot(epochs, self.val_epoch_errors, label='Validation Error', linewidth=2, color='blue')
 
         plt.xlabel('Epoch')
         plt.ylabel('Error')
