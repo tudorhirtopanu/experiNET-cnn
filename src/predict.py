14d6d6a696607451fa28ce2bfa3632d9f3fc7c91
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-09-04 21:59:41 +0100

    add predict.py

---
+++ src/predict.py
@@ -0,0 +1,19 @@
+from src.utils.load_model import load_model
+from src.utils.test_utils import preprocess_and_normalize, load_image
+
+image_path = "path/to/image.jpg"
+
+# Load the image
+input_image = load_image(image_path)
+
+# Preprocess the image
+preprocessed_image = preprocess_and_normalize(input_image)
+
+# Load the model and print the architecture
+model = load_model('model.h5')
+model.print_information()
+
+# Get confidence score and class name
+confidence, class_name = model.predict(preprocessed_image)
+
+print(f"Confidence: {confidence}, Class Name: {class_name}")
